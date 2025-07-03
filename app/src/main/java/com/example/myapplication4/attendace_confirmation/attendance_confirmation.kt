a0d27aa25a6ed314495076147b22744847740dd8
fionadhilla <fiona08dhilla@gmail.com>
2025-07-04 05:11:21 +0700

    ui

---
+++ app/src/main/java/com/example/myapplication4/attendace_confirmation/attendance_confirmation.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication4.attendace_confirmation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication4.R
+
+class attendance_confirmation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_attendance_confirmation)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
