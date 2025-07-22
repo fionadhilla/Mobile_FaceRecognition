d6baba34e809fd7e4ba1a3e973227ebaa6dad29d
fionadhilla <fiona08dhilla@gmail.com>
2025-07-22 08:10:32 +0700

    delet model user

--- app/src/main/java/com/example/myapplication4/data/model/User.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication4.data.model
-
-class User(
-    val fullName: String,
-    val email: String,
-    val phoneNumber: String
-)//TODO: ini nyambung ke profile mau diubah
